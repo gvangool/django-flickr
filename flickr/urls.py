18362ca3e9518986f8f72b91290eb827d6aff894
Gert Van Gool <gertvangool@gmail.com>
2019-08-17 20:13:48 +0200

    Switch to using callables in urls

--- flickr/urls.py
+++ flickr/urls.py
@@ -1,16 +1,22 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from flickr.views import IndexView, PhotoSetView
-
-urlpatterns = patterns('',
-    url(r'^auth/$', "flickr.views.oauth", name="flickr_auth"),
-    url(r'^auth/complete/$', "flickr.views.oauth_access", name="flickr_complete"),
-
-    url(r'^auth/deprecated/$', "flickr.views.auth", name="flickr_auth_deprecated"),
-    url(r'^flickr-callback/$', "flickr.views.auth", name="flickr_auth_callback"),
-
-    url(r'^method/(?P<method>.*)/$', "flickr.views.method_call", name="flickr_method"),
-    url(r'^set/(?P<flickr_id>.*)/$', PhotoSetView.as_view(), name="flickr_photoset"),
-    url(r'^photo/(?P<flickr_id>.*)/$', "flickr.views.photo", name="flickr_photo"),
-    url(r'^$', IndexView.as_view(), name="flickr_index"),
+from flickr.views import (
+    IndexView,
+    PhotoSetView,
+    auth,
+    method_call,
+    oauth,
+    oauth_access,
+    photo,
 )
+
+urlpatterns = [
+    url(r"^auth/$", oauth, name="flickr_auth"),
+    url(r"^auth/complete/$", oauth_access, name="flickr_complete"),
+    url(r"^auth/deprecated/$", auth, name="flickr_auth_deprecated"),
+    url(r"^flickr-callback/$", auth, name="flickr_auth_callback"),
+    url(r"^method/(?P<method>.*)/$", method_call, name="flickr_method"),
+    url(r"^set/(?P<flickr_id>.*)/$", PhotoSetView.as_view(), name="flickr_photoset"),
+    url(r"^photo/(?P<flickr_id>.*)/$", photo, name="flickr_photo"),
+    url(r"^$", IndexView.as_view(), name="flickr_index"),
+]
